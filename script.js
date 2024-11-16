c4e1592db1f97e2e8a440d83ace8843e2bb0a51d
Nurul Amin Khondoker <nurulaminkh33@gmail.com>
2024-11-16 16:35:44 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -42,13 +42,15 @@ const msg = document.getElementById("msg")
 
 form.addEventListener('submit', e => {
 e.preventDefault()
-fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => {
-      msg.innerHTML = "Message Submitted!"
-      setTimeout(function (){
-          msg.innerHTML = ""
-      }, 5000)
-      form.reset()
-  })
-  .catch(error => console.error('Error!', error.message))
+
+    msg.innerHTML = "Message Submitted!"
+    setTimeout(function (){
+      msg.innerHTML = ""
+    }, 5000)
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
 })
