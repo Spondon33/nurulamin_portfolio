15fe41bdedaeeb9a376123da1ce1b4e63bc4b394
Nurul Amin Khondoker <nurulaminkh33@gmail.com>
2024-11-16 16:26:30 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 var scrollTopBtn = document.getElementById("scrollTopBtn");
+var sidemenu = document.getElementById("sidemenu")
 
 function opentab(tabname){
     for(tablink of tablinks){
@@ -26,3 +27,28 @@ function scrollToTop() {
         behavior: "smooth"
     });
 }
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+// --- form submission ---
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxyLNHRQhSjXjRrk' +
+    'zH2808vJi5Uowa7sHuixKm8SXzBuRp_1uXmtnBH3mocngo_-0WxSA/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => {
+      msg.innerHTML = "Message Submitted!"
+      setTimeout(function (){
+          msg.innerHTML = ""
+      }, 5000)
+      form.reset()
+  })
+  .catch(error => console.error('Error!', error.message))
+})
